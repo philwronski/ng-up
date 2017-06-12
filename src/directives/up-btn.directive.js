33556f2b829c0955ef8f6d19fad04ef8030f898c
Philippe Wronski <philippe.wronski-ext@egis.fr>
2017-06-12 15:13:13 +0200

    Add support of double directive up-img and up-btn

--- src/directives/up-btn.directive.js
+++ src/directives/up-btn.directive.js
@@ -4,11 +4,13 @@ class UpBtnDirective {
         this.restrict = 'EA';
         this.scope = false;
         this.require = '^up';
-        this.template = '<input type="file" name="file" />';
+//        this.template = '<input type="file" name="file" />';
     }
     
     link(scope, element, attributes, ctrl) {
-        scope.input = angular.element(element[0].querySelector('[type="file"]'));
+        scope.input = angular.element('<input type="file" name="file" />');
+        element.append(scope.input);
+//        scope.input = angular.element(element[0].querySelector('[type="file"]'));
         scope.isMultiple = (attributes.multiple != undefined || attributes.multiple == "multiple");
         if(scope.isMultiple) {
             scope.input.attr('multiple', 'multiple');
@@ -17,19 +19,33 @@ class UpBtnDirective {
         scope.input.attr('accept', attributes.accept);
         element.bind("change", (event) => {
             if(scope.isMultiple) {
-                scope.files = scope.$parent.files;
+//                scope.files = scope.$parent.files;
                 scope.$apply(function() {
                     _.forEach(event.target.files, (file) => {
                         ctrl.addFile(file);
                     });
+                    scope.files = ctrl.files;
                 });
             } else {
                 scope.$apply(function() {
                     ctrl.files[0] = event.target.files[0];
+                    scope.files = ctrl.files;
                 });
             }
             scope.input.val('');
         });
+        
+        if(attributes.upBtn != undefined && attributes.upImg != undefined) {
+            let preview = angular.element(element[0].querySelector('.up-img-preview'));
+            preview.bind('click', (event) => {
+               scope.input[0].click();
+            });
+        } else {
+            element.bind('click', (event) => {
+               scope.input[0].click();
+            });
+        }
+        
     }
     
     static directiveFactory(){
